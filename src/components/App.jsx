8245898a93b1ac5280909f1a7e47cdc67e79f1ff
Igor9779 <darthhoit97@gmail.com>
2023-09-07 21:37:33 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,7 @@ export class App extends Component {
       // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
     ],
     filter: ''
-  };
+  }
 
   formSubmit = ({ name, number }) => {
     const contact = {
